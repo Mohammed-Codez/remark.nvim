9c0a0a3a5e20dc0f99b5272c586ab2016268142b
Mohammed-Codez <amakactive@gmail.com>
2026-07-10 19:00:10 +0400

    removed config param in setup func

--- lua/remark/init.lua
+++ lua/remark/init.lua
@@ -3,7 +3,7 @@ local M = {}
 local palette = require("remark.palette")
 local highlights = require("remark.highlights")
 
-function M.setup(config)
+function M.setup()
 	vim.cmd("hi clear")
 
 	if vim.fn.exists("syntax on") then
