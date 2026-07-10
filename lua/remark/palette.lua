cdd931360336ccb31c2633e2c2cae71894c7f8ba
Mohammed-Codez <amakactive@gmail.com>
2026-07-10 15:40:41 +0400

    add range of shade

--- lua/remark/palette.lua
+++ lua/remark/palette.lua
@@ -1,6 +1,12 @@
 local palette = {
-	black = "#222222",
-	white = "#eeeeee",
+	black = "#202020",
+	gray1 = "#3c3c3c",
+	gray2 = "#585858",
+	gray3 = "#747474",
+	gray4 = "#909090",
+	gray5 = "#acacac",
+	gray6 = "#c8c8c8",
+	white = "#e4e4e4",
 }
 
 local variants = {
