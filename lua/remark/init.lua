af119734a2c9e9ba91a590ce520d227a0ac8669e
Mohammed-Codez <amakactive@gmail.com>
2026-07-11 11:41:16 +0400

    added config capabilities. will see if works

--- lua/remark/init.lua
+++ lua/remark/init.lua
@@ -3,7 +3,10 @@ local M = {}
 local palette = require("remark.palette")
 local highlights = require("remark.highlights")
 
-function M.setup()
+function M.setup(user_config)
+	user_config = user_config or {}
+	M.config = vim.tbl_deep_extend("force", M.config, user_config)
+
 	vim.cmd("hi clear")
 
 	if vim.fn.exists("syntax on") then
