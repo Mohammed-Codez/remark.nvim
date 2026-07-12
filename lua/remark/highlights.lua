6e50415c68c6f29777ca8d9bf8be0f7f3e7bd87b
Mohammed-Codez <amakactive@gmail.com>
2026-07-12 20:25:01 +0400

    added more colors. also changed greens

--- lua/remark/highlights.lua
+++ lua/remark/highlights.lua
@@ -5,9 +5,11 @@ function M.set(palette)
 
 	local groups = {
 		Conceal = { fg = palette.gray2 },
-		CurSearch = { fg = palette.bg, bg = palette.fg },
-		IncSearch = { link = "CurSearch" },
-		Cursor = { link = "CurSearch" },
+		CurSearch = { link = "Cursor" },
+		Search = { fg = palette.bg, bg = palette.gray3 },
+		IncSearch = { link = "Cursor" },
+		Substitute = { link = "Search" },
+		Cursor = { fg = palette.bg, bg = palette.fg },
 		TermCursor = { link = "Cursor" },
 		CursorLine = { bg = palette.gray1 },
 		CursorColumn = { link = "CursorLine" },
@@ -17,12 +19,22 @@ function M.set(palette)
 		ErrorMsg = { fg = palette.lred },
 		StderrMsg = { link = "ErrorMsg" },
 		WinSeperator = { link = "Normal" },
+		LineNr = { link = "Conceal" },
+		CursorLineNr = { link = "Cursor" },
+		MatchParen = { link = "Cursor" },
+		MsgArea = { link = "Normal" },
 		Normal = { fg = palette.fg, bg = palette.bg },
+		Pmenu = { link = "Normal" },
+		PmenuSel = { link = "Cursor" },
+		PmenuThumb = { link = "Cursor" },
+		PmenuBorder = { link = "WinSeperator" },
+		StatusLine = { link = "Normal" },
+		StatusLineTerm = { link = "StatusLine" },
+		Visual = { bg = palette.gray2 },
+		Whitespace = { fg = palette.gray4 },
 	}
 
 	for group, highlight in pairs(groups) do
-		print(group)
-		print(highlight)
 		set(0, group, highlight)
 	end
 end
