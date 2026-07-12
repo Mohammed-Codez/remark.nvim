dbc471c1e196849e51bf6e38facd81ca339b00f0
Mohammed-Codez <amakactive@gmail.com>
2026-07-12 16:35:04 +0400

    updated palette, as well as added actual hues

--- lua/remark/highlights.lua
+++ lua/remark/highlights.lua
@@ -4,7 +4,20 @@ function M.set(palette)
 	local set = vim.api.nvim_set_hl
 
 	local groups = {
-		Normal = { fg = palette.white, bg = palette.black },
+		Conceal = { fg = palette.gray2 },
+		CurSearch = { fg = palette.bg, bg = palette.fg },
+		IncSearch = { link = "CurSearch" },
+		Cursor = { link = "CurSearch" },
+		TermCursor = { link = "Cursor" },
+		CursorLine = { bg = palette.gray1 },
+		CursorColumn = { link = "CursorLine" },
+		Directory = { fg = palette.gray6 },
+		OkMsg = { fg = palette.lgreen },
+		WarningMsg = { fg = palette.lyellow },
+		ErrorMsg = { fg = palette.lred },
+		StderrMsg = { link = "ErrorMsg" },
+		WinSeperator = { link = "Normal" },
+		Normal = { fg = palette.fg, bg = palette.bg },
 	}
 
 	for group, highlight in pairs(groups) do
