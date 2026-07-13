a8363e14ea9684bcb65140e5b12071a2b27c2cb9
Mohammed-Codez <amakactive@gmail.com>
2026-07-13 18:12:36 +0400

    darkened yellows

--- lua/remark/palette.lua
+++ lua/remark/palette.lua
@@ -15,8 +15,8 @@ local palette = {
 	lred = "#d08080",
 	green = "#849070",
 	lgreen = "#98b090",
-	yellow = "#c8b880",
-	lyellow = "#e0d090",
+	yellow = "#c0b070",
+	lyellow = "#d0c080",
 	blue = "#6880a0",
 	lblue = "#7490c0",
 }
