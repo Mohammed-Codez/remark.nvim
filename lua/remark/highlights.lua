140c3c1b807d3c5a02f77a9d0a621386a259772a
Mohammed-Codez <amakactive@gmail.com>
2026-07-13 19:08:50 +0400

    shifted terminal color settings into init.lua

--- lua/remark/highlights.lua
+++ lua/remark/highlights.lua
@@ -79,23 +79,6 @@ function M.set(palette)
 	for group, highlight in pairs(groups) do
 		set(0, group, highlight)
 	end
-
-	vim.g.terminal_colors_0 = palette.black
-	vim.g.terminal_colors_1 = palette.red
-	vim.g.terminal_colors_2 = palette.green
-	vim.g.terminal_colors_3 = palette.yellow
-	vim.g.terminal_colors_4 = palette.blue
-	vim.g.terminal_colors_5 = palette.magenta
-	vim.g.terminal_colors_6 = palette.cyan
-	vim.g.terminal_colors_7 = palette.gray5
-	vim.g.terminal_colors_8 = palette.gray2
-	vim.g.terminal_colors_9 = palette.lred
-	vim.g.terminal_colors_10 = palette.lgreen
-	vim.g.terminal_colors_11 = palette.lyellow
-	vim.g.terminal_colors_12 = palette.lblue
-	vim.g.terminal_colors_13 = palette.lmagenta
-	vim.g.terminal_colors_14 = palette.lcyan
-	vim.g.terminal_colors_15 = palette.white
 end
 
 return M
--- lua/remark/init.lua
+++ lua/remark/init.lua
@@ -18,6 +18,23 @@ function M.setup(user_config)
 	end
 
 	vim.g.colors_name = "remark-" .. M.config.style
+
+	vim.g.terminal_colors_0 = palette.black
+	vim.g.terminal_colors_1 = palette.red
+	vim.g.terminal_colors_2 = palette.green
+	vim.g.terminal_colors_3 = palette.yellow
+	vim.g.terminal_colors_4 = palette.blue
+	vim.g.terminal_colors_5 = palette.magenta
+	vim.g.terminal_colors_6 = palette.cyan
+	vim.g.terminal_colors_7 = palette.gray5
+	vim.g.terminal_colors_8 = palette.gray2
+	vim.g.terminal_colors_9 = palette.lred
+	vim.g.terminal_colors_10 = palette.lgreen
+	vim.g.terminal_colors_11 = palette.lyellow
+	vim.g.terminal_colors_12 = palette.lblue
+	vim.g.terminal_colors_13 = palette.lmagenta
+	vim.g.terminal_colors_14 = palette.lcyan
+	vim.g.terminal_colors_15 = palette.white
 	highlights.set(palette.get(M.config.style))
 end
 
